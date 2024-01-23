a44bcfc983f1514565fa46cdd3afa74a2ebcda6d
ArchEvan03 <etbagwell03@gmail.com>
2024-01-22 21:55:40 -0600

    func implemented
    (cherry picked from commit 2038720aef86ce9dbf02ef531579d60d0ef3b1f4)

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
     const logButton = document.getElementById("log");
-    const logAdd = document.getElementById("add");
-
     logButton.addEventListener("click", function() {
         console.log("The button was pressed!");
     }) 
-    logAdd.addEventListener("click", function() {
-        
-    })
 
 });
+
+async function addName() {
+    const name = document.getElementById("name").value;
+    let table = document.getElementById("guestTable");
+    let row = table.insertRow();
+    let cell = row.insertCell();
+    cell.innerHTML = name;
+}
