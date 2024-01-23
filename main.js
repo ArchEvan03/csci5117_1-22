5aa017ee5cb0d7d57acc6c407a6422e68a43cea1
ArchEvan03 <etbagwell03@gmail.com>
2024-01-22 21:27:29 -0600

    framework added

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const logButton = document.getElementById("log");
+    const logAdd = document.getElementById("add");
+
+    logButton.addEventListener("click", function() {
+        console.log("The button was pressed!");
+    }) 
+    logAdd.addEventListener("click", function() {
+        
+    })
+
+});
